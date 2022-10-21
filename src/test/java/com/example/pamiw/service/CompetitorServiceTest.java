34890c5cbed3994190b51dfae684cd153e8e898f
ojAntoni <zezyulin90@gmail.com>
2022-10-21 18:58:19 +0300

    implementation for save(Competitor) test

--- src/test/java/com/example/pamiw/service/CompetitorServiceTest.java
+++ src/test/java/com/example/pamiw/service/CompetitorServiceTest.java
@@ -1,14 +1,15 @@
 package com.example.pamiw.service;
 
+import com.example.pamiw.domain.Competitor;
 import com.example.pamiw.repository.CompetitorRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 class CompetitorServiceTest {
 
@@ -27,6 +28,20 @@ class CompetitorServiceTest {
 
     @Test
     void save() {
+        AtomicInteger index = new AtomicInteger();
+        Mockito.when(mock.save(ArgumentMatchers.notNull())).
+                then(invocation ->
+                    Competitor.builder().id(index.incrementAndGet()).build()
+                    );
+
+        Competitor competitor = new Competitor();
+        int id = 0;
+        for(int i = 0; i < 10; i++){
+            int returnedIndex = competitorService.save(competitor);
+            Assertions.assertEquals(++id, returnedIndex);
+        }
+
+        Mockito.verify(mock, Mockito.times(10)).save(ArgumentMatchers.notNull());
     }
 
     @Test
